d16db39c7ed6dd89925cea1f3432b9449bd16b72
RISHABHXVAZ <srivastavarishabh799@gmail.com>
2026-02-09 01:16:22 +0530

    Setted up backend

---
+++ backend/src/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+/*
+   MIDDLEWARE
+*/
+app.use(cors());
+app.use(express.json());
+
+/*
+   TEST ROUTE
+*/
+app.get("/", (req, res) => {
+    res.send("AI Expense Tracker API Running");
+});
+
+/*
+   START SERVER
+*/
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
